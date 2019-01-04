9b8040b71ccc378011672ed9e14f54d2db4866ce
babytech <babytech@126.com>
2019-01-04 20:39:57 +0800

    gfifo: support platform device and platform driver

---
+++ gfifo_platform/gfifo_platform_device.c
@@ -0,0 +1,53 @@
+/*
+ * a simple platform device for gfifo
+ */
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/platform_device.h>
+
+static struct platform_device gfifo_platform_device = {
+	.name = "gfifo",
+	.id = -1,
+};
+
+static int __init gfifo_device_init(void)
+{
+	return platform_device_register(&gfifo_platform_device);
+}
+module_init(gfifo_device_init);
+
+static void __exit gfifo_device_exit(void)
+{
+	platform_device_unregister(&gfifo_platform_device);
+}
+module_exit(gfifo_device_exit);
+
+/*
+static struct platform_device *gfifo_pdev;
+
+static int __init gfifo_device_init(void)
+{
+	int ret;
+
+	gfifo_pdev = platform_device_alloc("gfifo", -1);
+	if (!gfifo_pdev)
+		return -ENOMEM;
+
+	ret = platform_device_add(gfifo_pdev);
+	if (ret) {
+		platform_device_put(gfifo_pdev);
+		return ret;
+	}
+	return 0;
+}
+module_init(gfifo_device_init);
+
+static void __exit gfifo_device_exit(void)
+{
+	platform_device_unregister(gfifo_pdev);
+}
+module_exit(gfifo_device_exit);
+*/
+
+MODULE_AUTHOR("babytech@126.com");
+MODULE_LICENSE("GPL v2");
